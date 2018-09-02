7d97817ef5563da052221204b98002517828dc6d
Claire Longo <longoclaire@gmail.com>
2018-09-02 11:24:23 -0600

    clean up similarity method to make it more robust

--- movie_recommender/similarity.py
+++ movie_recommender/similarity.py
@@ -2,26 +2,39 @@ from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 
 class SimilarityPredictions(object):
-    '''This class contains a method for calculating a similairty matrix from
-    latent embeddings,and a method for predicting similar items from the
-    similarity matrix.
-    Input: embeddings - a pandas dataframe of items and latent dimensions.'''
+    '''This class calculates a similarity matrix from latent embeddings.
+    There is a method to save this similarity model locally, and a method for
+    predicting similar items from the matrix.
+    Input: embeddings - a pandas dataframe of items and latent dimensions.
+            similarity_metric = str definining the similarity metrics to use'''
 
-    def __init__(self, embeddings):
+    def __init__(self, embeddings, similarity_metric='cosine'):
+        assert similarity_metric in ['cosine'], "unsupported similarity metric."
         self.embeddings = embeddings
         self.ids = embeddings.index.tolist()
+        if similarity_metric == 'cosine':
+            self.similarity_matrix = self.calculate_cosine_similarity_matrix()
 
-    def calculate_similarity_matrix(self):
+    def calculate_cosine_similarity_matrix(self):
         '''Calculates a cosine similarity matrix from the embeddings'''
         similarity_matrix = pd.DataFrame(cosine_similarity(
             X=self.embeddings),
-            index=movie_id)
+            index=self.ids)
         similarity_matrix.columns = self.ids
         return similarity_matrix
 
-    def predict_similar_items(self, seed_item, n, similarity_matrix):
-        '''Use the similarity_matrix to return n most similar items'''
-        similar_items = pd.DataFrame(similarity_matrix.loc[seed_item])
+    def save_similarity_model(self, path, file_format='csv'):
+        '''Save similarity matrix locally'''
+        assert file_format in ['csv', 'pickle'], "unsupported format"
+        if file_format == 'csv':
+            self.similarity_matrix.to_csv(path, header=True, index=True)
+        elif forfile_formatmat == 'pickle':
+            self.similarity_matrix.to_pickle(path)
+
+    def predict_similar_items(self, seed_item, n):
+        '''Use the similarity_matrix to return n most similar items.
+        '''
+        similar_items = pd.DataFrame(self.similarity_matrix.loc[seed_item])
         similar_items.columns = ["similarity_score"]
         similar_items = similar_items.sort_values('similarity_score', ascending=False)
         similar_items = similar_items.head(n)
