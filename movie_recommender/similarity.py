3f4a31286b80f2d8ad700d8d3ab557896517b9b6
Claire Longo <longoclaire@gmail.com>
2018-09-09 13:17:02 -0600

    wip

--- movie_recommender/similarity.py
+++ movie_recommender/similarity.py
@@ -35,17 +35,8 @@ class SimilarityPredictions(object):
         similarity_matrix.columns = self.ids
         return similarity_matrix
 
-    def save_similarity_model(self, path, file_format='csv'):
-        '''Save similarity matrix locally'''
-        assert file_format in ['csv', 'pickle'], "unsupported format"
-        if file_format == 'csv':
-            self.similarity_matrix.to_csv(path, header=True, index=True)
-        elif file_format == 'pickle':
-            self.similarity_matrix.to_pickle(path)
-
     def predict_similar_items(self, seed_item, n):
-        '''Use the similarity_matrix to return n most similar items.
-        '''
+        '''Use the similarity_matrix to return n most similar items.'''
         similar_items = pd.DataFrame(self.similarity_matrix.loc[seed_item])
         similar_items.columns = ["similarity_score"]
         if self.similarity_metric == 'cosine':
