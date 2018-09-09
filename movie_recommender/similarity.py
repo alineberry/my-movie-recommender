2a07274a9d73c6f416b87605b0962634fd64941b
Claire Longo <longoclaire@gmail.com>
2018-09-09 11:12:58 -0600

    similar movie results

--- movie_recommender/similarity.py
+++ movie_recommender/similarity.py
@@ -10,11 +10,14 @@ class SimilarityPredictions(object):
             similarity_metric = str definining the similarity metrics to use'''
 
     def __init__(self, embeddings, similarity_metric='cosine'):
-        assert similarity_metric in ['cosine'], "unsupported similarity metric."
+        assert similarity_metric in ['cosine', 'euclidean'], "unsupported similarity metric."
         self.embeddings = embeddings
         self.ids = embeddings.index.tolist()
+        self.similarity_metric = similarity_metric
         if similarity_metric == 'cosine':
             self.similarity_matrix = self.calculate_cosine_similarity_matrix()
+        if similarity_metric == 'euclidean':
+            self.similarity_matrix = self.calculate_euclidean_distances_matrix()
 
     def calculate_cosine_similarity_matrix(self):
         '''Calculates a cosine similarity matrix from the embeddings'''
@@ -26,11 +29,11 @@ class SimilarityPredictions(object):
 
     def calculate_euclidean_distances_matrix(self):
         '''Calculates a cosine similarity matrix from the embeddings'''
-        euclisimilarity_matrixdean_matrix = pd.DataFrame(euclidean_distances(
+        similarity_matrix= pd.DataFrame(euclidean_distances(
             X=self.embeddings),
             index=self.ids)
         similarity_matrix.columns = self.ids
-        return euclsimilarity_matrixidean_matrix
+        return similarity_matrix
 
     def save_similarity_model(self, path, file_format='csv'):
         '''Save similarity matrix locally'''
@@ -45,7 +48,10 @@ class SimilarityPredictions(object):
         '''
         similar_items = pd.DataFrame(self.similarity_matrix.loc[seed_item])
         similar_items.columns = ["similarity_score"]
-        similar_items = similar_items.sort_values('similarity_score', ascending=False)
+        if self.similarity_metric == 'cosine':
+            similar_items = similar_items.sort_values('similarity_score', ascending=False)
+        if self.similarity_metric == 'euclidean':
+            similar_items = similar_items.sort_values('similarity_score', ascending=True)
         similar_items = similar_items.head(n)
         similar_items.reset_index(inplace=True)
         similar_items = similar_items.rename(index=str, columns={"index": "item_id"})
